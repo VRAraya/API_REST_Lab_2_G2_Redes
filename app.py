d12049fc9b7476e7c476b98e2d3b1e3295941cf9
VRAraya <victor.arayar@utem.cl>
2019-10-06 16:23:06 -0300

    Construccion de la API y verificador de digito rut listo.

---
+++ app.py
@@ -0,0 +1,42 @@
+# Se usa flask para construir la API 
+from flask import Flask, jsonify, request
+from itertools import cycle
+
+app = Flask(__name__)
+
+# la funcion calcula el digito verificador según un rut
+def digVerificator (rut):
+  reversed_digits = map(int, reversed(rut))
+  factors = cycle(range(2, 8))
+  s=sum(d*f for d, f in zip (reversed_digits, factors))
+  return (-s) % 11
+
+# on the terminal type: curl http://127.0.0.1:5000/ 
+# returns hello world when we use GET. 
+# returns the data that we send when we use POST. 
+@app.route('/', methods = ['GET', 'POST']) 
+def home(): 
+	if(request.method == 'GET'): 
+
+		data = "hello world"
+		return jsonify({'data': data}) 
+
+
+# Una funcion para verificar el digito proporcionado junto con el rut en el formato xx.xxx.xxx-x
+@app.route('/RUT/<string:rutd>', methods = ['GET'])
+def verifyRut(rutd):
+  strRutd = rutd.split('-')
+  dig = digVerificator(strRutd[0])
+
+  if(dig==10 and strRutd[1]=='k'):
+    return jsonify({'isVerify': True}) 
+  
+  if(int(strRutd[1]) == dig):
+    return jsonify({'isVerify': True})
+
+  return jsonify({'isVerify': False}) 
+
+
+# funcion driver
+if __name__ == '__main__': 
+	app.run(debug = True) 
